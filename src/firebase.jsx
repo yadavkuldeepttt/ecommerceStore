8f2973fdb70e0048974144b727b879c7224e3985
Kuldeep <yadavkuldeepdell@gmail.com>
2023-09-14 17:41:50 +0530

    Firebase Rules Updated

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -5,13 +5,20 @@ import "firebase/compat/storage";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBjrjxc8h8kfilOghHjdKHuITAdzm9EysQ",
-  authDomain: "yadavcart-c8410.firebaseapp.com",
-  projectId: "yadavcart-c8410",
-  storageBucket: "yadavcart-c8410.appspot.com",
-  messagingSenderId: "346151247541",
-  appId: "1:346151247541:web:c2b23616aadd4db1f6e8fc",
-  measurementId: "G-Y2LPE9MT7T",
+  // apiKey: "AIzaSyBjrjxc8h8kfilOghHjdKHuITAdzm9EysQ",
+  // authDomain: "yadavcart-c8410.firebaseapp.com",
+  // projectId: "yadavcart-c8410",
+  // storageBucket: "yadavcart-c8410.appspot.com",
+  // messagingSenderId: "346151247541",
+  // appId: "1:346151247541:web:c2b23616aadd4db1f6e8fc",
+  // measurementId: "G-Y2LPE9MT7T",
+  apiKey: "AIzaSyAu6gpCyabdClFd4FilbD5hkpFKuQHzS1o",
+  authDomain: "ecommerce-a9a47.firebaseapp.com",
+  projectId: "ecommerce-a9a47",
+  storageBucket: "ecommerce-a9a47.appspot.com",
+  messagingSenderId: "873379430978",
+  appId: "1:873379430978:web:fe860c94b2edd4520a23db",
+  measurementId: "G-V3WJ9VT5QN",
 };
 
 // Initialize Firebase
